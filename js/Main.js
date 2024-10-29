a61336818042325116491510281c4556da8b857d
Rebin Ismail <rebin@MacBook-Pro-tilhrende-Rebin.local>
2024-10-29 09:16:06 +0100

    Init

---
+++ js/Main.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            intro: 'Welcome to my Vue template',
+        }
+    },
+    methods: {
+        myMethod(){
+        },
+    },
+    computed: {
+        myComputed() {
+            return ""
+        },
+    }
+})
